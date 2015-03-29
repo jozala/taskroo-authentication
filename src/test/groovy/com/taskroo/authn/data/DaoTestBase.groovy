41307e9c8bbe10a4277e201f4978ad69328e77f9
mariusz <mariusz@aetas.pl>
2015-03-30 01:49:16 +0200

    Tests which use Mongo corrected to take parameters from system environment when possible

--- src/test/groovy/com/taskroo/authn/data/DaoTestBase.groovy
+++ src/test/groovy/com/taskroo/authn/data/DaoTestBase.groovy
@@ -6,7 +6,8 @@ import spock.lang.Specification
 
 class DaoTestBase extends Specification {
 
-    private static final DB db = new MongoConnector('localhost', '27017').getDatabase('taskroo-dao-tests-db')
+    private static final DB db = new MongoConnector(System.properties.getProperty('MONGO_PORT_27017_TCP_ADDR', 'localhost'),
+            System.properties.getProperty('MONGO_PORT_27017_TCP_PORT', '27017')).getDatabase('taskroo-dao-tests-db')
     public static final DBCollection usersCollection = db.getCollection('users')
     public static final DBCollection securityTokensCollection = db.getCollection('securityTokens')
     public static final DBCollection rememberMeTokensCollection = db.getCollection('rememberMeTokens')
